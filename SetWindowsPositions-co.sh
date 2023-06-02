4d368c61f9747725aa98ed5f2ab216fb27f4dff9
Andreas Pankoke <apankoke@comprion.com>
2023-06-02 18:01:34 +0200

    put the sheban /bin/bash in front of the scripts

---
+++ SetWindowsPositions-co.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# start devilspie2 to set window positions
+# positions are scripted with lua in ~/.config/devilspie2/debug.lua
+# Die aktuellen Fenster Positionen werden in dieser Datei gespeichert/home/apankoke/devilspie2output.txt
+# apankoke@10/2022
+devilspie2 --folder=/home/apankoke/devilspie-scripts-hd --debug > /home/apankoke/devilspie-scripts-hd/output
