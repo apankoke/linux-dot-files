a71a11aa2e0c7e28b8589cf296f742f5e55f6508
Andreas Pankoke <apankoke@comprion.com>
2023-06-12 12:02:21 +0200

    fix the last things! hopefuly!

--- postbuildscripts/compressrootfs.sh
+++ postbuildscripts/compressrootfs.sh
@@ -73,7 +73,7 @@ if [ "$compressrootfs" = true ] ; then
     echo "tar $ImagesPath/$Updatefilename to $ImagesPath"
    # zip -r $RootfsPath/$Updatefilename $RootfsPath
     #tar -zcvf [result-filename.tar.gz] [path-of-directory-to-compress]
-    tar -zcvf $ImagesPath/$Updatefilename $ImagesPath
+    tar -zcvf -exclude=$UpdateFilename $ImagesPath/$Updatefilename $ImagesPath 
       
     echo -e "\n------------------------ finished ------------------------------------------\n"
 fi
